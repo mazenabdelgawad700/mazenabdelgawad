bced2ab59536fcd470f0d0a37f6dc3ae7354e01b
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-04-03 16:19:49 +0200

    change the favicon

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Contact from "../../Components/Contact/Contact";
 import Footer from "../../Components/Footer/Footer";
 import Hero from "../../Components/Hero/Hero";
 import Portfolio from "../../Components/Portfolio/Portfolio";
-import "./Home.css";
+
 const Home = () => {
   return (
     <div>
