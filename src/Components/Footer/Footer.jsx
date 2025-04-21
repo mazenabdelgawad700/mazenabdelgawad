60502187533d217944c1ce3af86e3096bf189e32
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-04-21 17:34:29 +0200

    update footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaFacebookSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -21,6 +21,13 @@ const Footer = () => {
             href="https://github.com/mazenabdelgawad700"
           >
             <FaGithub />
+          </a>
+          <a
+            target="_blank"
+            className="mx-1 text-light"
+            href="https://www.facebook.com/profile.php?id=61559125871310"
+          >
+            <FaFacebookSquare />
           </a>
         </p>
       </div>
